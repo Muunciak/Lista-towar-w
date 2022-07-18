4737305a06a3404c191edf1ac373ebd08de5a695
Munciak <MaariioO58@gmail.com>
2022-07-18 09:27:10 +0200

    Add project files.

---
+++ Lista towarów/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lista_towarów
+{
+    partial class csvReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDataList = new System.Windows.Forms.DataGridView();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnEditCode = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataList)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDataList
+            // 
+            this.dgvDataList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataList.Location = new System.Drawing.Point(12, 12);
+            this.dgvDataList.Name = "dgvDataList";
+            this.dgvDataList.Size = new System.Drawing.Size(776, 384);
+            this.dgvDataList.TabIndex = 0;
+            this.dgvDataList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDataList_CellContentClick);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(652, 402);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(136, 36);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Znajdz plik";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnEditCode
+            // 
+            this.btnEditCode.Location = new System.Drawing.Point(510, 402);
+            this.btnEditCode.Name = "btnEditCode";
+            this.btnEditCode.Size = new System.Drawing.Size(136, 36);
+            this.btnEditCode.TabIndex = 4;
+            this.btnEditCode.Text = "Zmień kod";
+            this.btnEditCode.UseVisualStyleBackColor = true;
+            this.btnEditCode.Click += new System.EventHandler(this.btnEditCode_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(100, 17);
+            this.toolStripStatusLabel1.Text = "Status: Połączono";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(110, 17);
+            this.toolStripStatusLabel2.Text = "Baza: Lista towarów";
+            // 
+            // csvReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnEditCode);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.dgvDataList);
+            this.Name = "csvReader";
+            this.Text = "csvReader";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataList)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDataList;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnEditCode;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ Lista towarów/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+
+namespace Lista_towarów
+{
+    public partial class csvReader : Form
+    {
+        public csvReader()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            {
+                OpenFileDialog opf = new OpenFileDialog();
+                opf.Filter = "csv File|*.csv| Excel Files(.xls)|*.xls| Excel Files(.xlsx)| *.xlsx | Excel Files(*.xlsm) | *.xlsm";
+                if (opf.ShowDialog() == DialogResult.OK)
+                {
+                    string[] Lines = File.ReadAllLines(opf.FileName);
+                    string[] Fields;
+                    Fields = Lines[0].Split(new char[] { ';' });
+                    int Cols = Fields.GetLength(0);
+                    DataTable dt = new DataTable();
+                    for (int i = 0; i < Cols; i++)
+                        dt.Columns.Add(Fields[i].ToLower(), typeof(string));
+                    DataRow Row;
+                    for (int i = 1; i < Lines.GetLength(0); i++)
+                    {
+                        Fields = Lines[i].Split(new char[] { ';' });
+                        Row = dt.NewRow();
+                        for (int f = 0; f < Cols; f++)
+                            Row[f] = Fields[f];
+                        dt.Rows.Add(Row);
+                    }
+                    dgvDataList.DataSource = dt;
+                }
+            }
+        }
+        private void dgvDataList_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnEditCode_Click(object sender, EventArgs e)
+        {
+            int selectedRowIndex = dgvDataList.CurrentRow.Index;
+            int columnIndex = dgvDataList.CurrentCell.ColumnIndex;
+
+            if (columnIndex == 3)
+            {
+                dgvDataList.BeginEdit(true);
+            }
+            else
+            {
+                MessageBox.Show("Mozesz edytowac tylko kod!");
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
